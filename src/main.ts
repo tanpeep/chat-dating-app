672175edf13b0e0a624f046ad9dd9af3ccdd17b5
Sulthan Afif Althaf <sulthan.afif@ui.ac.id>
2023-05-23 06:37:05 +0700

    enable CORS

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as bodyParser from 'body-parser';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(cors());
+  app.enableCors();
   await app.listen(3000);
 
   const secondapp = await NestFactory.create(AppModule);
   secondapp.use(bodyParser.urlencoded({ extended: true}));
+  secondapp.use(cors());
+  secondapp.enableCors();
   await secondapp.listen(3010);
 }
 bootstrap();
